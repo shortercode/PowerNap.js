1cc3e83bc8a2556a5893f6cd8a1110b8bce9046d
shortercode <iainshorter@Iains-MacBook-Pro.local>
2016-02-26 10:45:14 +0000

    Resolved test file

--- test.js
+++ test.js
@@ -4,8 +4,8 @@ app.endpoint("get", "/Tests", function (req, res) {
 	res.end("You smell");
 });
 
-app.staticEndpoint("/public", "./FolderA");
-
 app.endpoint("get", "/hello/:world/test/:lemon", function (req, res) {
 	res.end(JSON.stringify(req.parameters, null, 2));
 });
+
+app.staticEndpoint("/Public", "../");
